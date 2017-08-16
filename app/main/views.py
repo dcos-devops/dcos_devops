df5a0810bdf45ab2d07a04eaa1b752c8cc35135d
sunhongtao <sighofdead@163.com>
2017-08-16 14:28:07 +0800

    #Fix:修复重复添加thead

--- app/main/views.py
+++ app/main/views.py
@@ -3,7 +3,7 @@ from flask import render_template, request, jsonify
 import pymysql
 from app.models import Icinga_info
 from app.db import db_init 
-
+import json,urllib
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
@@ -68,9 +68,67 @@ def get_service_time():
 
 @main.route('/hello1', methods=['GET', 'POST'])
 def hello1():
-    return render_template("hello1.html")
+    return render_template("hello1.html",mesos=mesos)
 
 
 @main.route('/hello2', methods=['GET', 'POST'])
 def hello2():
-    return render_template("hello2.html")
+    dmz_mesos_ips=["20.26.28.22:5050","20.26.28.23:5050","20.26.28.24:5050"]
+    dmz_marathon_ips=["20.26.28.25","20.26.28.26"]
+    dmz_mesos=[]
+    # mesos=[("mesos-master","http://192.168.0.1:5050","No",1),("mesos-master","192.168.0.2","No",1),("mesos-master","192.168.0.3","No",0)]
+    marathon=[]
+    dmz_mesos_leader = get_mesos_leader(dmz_mesos_ips)
+    for mesosip in dmz_mesos_ips:
+        obj = MesosChecker(mesosip.split(":")[0],mesosip.split(":")[1])
+        _is_ok = obj.check()
+        _is_leader = ["No" if dmz_mesos_leader.split("//")[-1].strip('/') != mesosip else "Yes"][0]
+        info = ("mesos-master","http://{}".format(mesosip),_is_leader,_is_ok)
+        dmz_mesos.append(info)
+    
+    # dmz_maraton_leader = get_marathon_leader(dmz_marathon_ips)
+    
+    
+
+
+    return render_template("hello2.html",mesos=dmz_mesos,marathon=marathon)
+
+
+
+class MesosChecker:
+    def __init__(self, ip, port):
+        self.ip = ip
+        self.port = port
+
+    def _get_url(self):
+        return 'http://{}:{}/metrics/snapshot'.format(self.ip, self.port)
+
+    def check(self):
+        try:
+            node = self._get_url()
+            response = urllib.request.urlopen(node, timeout=10)
+            return True
+        except:
+            return False
+
+
+def get_mesos_leader(mesos_master_nodes):
+    for mmn in mesos_master_nodes:
+        baseurl = None
+        try:
+            url = "http://{0}/redirect".format(mmn)
+            baseurl = get_redirected_url(url)
+        except Exception as e:
+            logger.error("failed to get url: {} ".format(url))
+            logger.exception(e)
+        if baseurl:
+            break
+
+    if baseurl is None:
+        raise Exception
+    return baseurl
+
+def get_redirected_url(url):
+    opener = urllib.request.build_opener(urllib.request.HTTPRedirectHandler)
+    request = opener.open(url, timeout=10)
+    return request.url
